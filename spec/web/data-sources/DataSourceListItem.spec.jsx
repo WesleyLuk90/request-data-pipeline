a61853f18306752c41a239a5a1e636122693ed89
Wesley Luk <wesleyluk@gmail.com>
2016-11-23 22:28:02 -0800

    Updated formatting of data sources list

--- spec/web/data-sources/DataSourceListItem.spec.jsx
+++ spec/web/data-sources/DataSourceListItem.spec.jsx
@@ -1,12 +1,37 @@
 const React = require('react');
 const { shallow } = require('enzyme');
 
+const RestDataSource = require('../../../src/shared/RestDataSource');
 const DataSourceListItem = require('../../../src/web/data-sources/DataSourceListItem');
+const Module = require('../../../src/web/Module');
+const MockEvent = require('../MockEvent');
 
 describe('DataSourceListItem', () => {
+    let module;
+    let dataSource;
+    let component;
+
+    beforeEach(() => {
+        module = new Module({ router: jasmine.createSpyObj('router', ['go']) });
+        dataSource = new RestDataSource();
+        dataSource.id = 'id';
+        dataSource.name = 'Name';
+        dataSource.url = 'http://url';
+
+        component = shallow(<DataSourceListItem dataSource={dataSource} module={module} />);
+    });
+
     it('should display', () => {
-        const component = shallow(<DataSourceListItem />);
         expect(component).toBeDefined();
         expect(component.find('.data-source-list-item')).toBeDefined();
+
+        expect(component.find('.data-source-list-item__name').text()).toBe(dataSource.name);
+        expect(component.find('.data-source-list-item__url').text()).toBe(dataSource.url);
+    });
+
+    it('should go to the edit page', () => {
+        component.find('.data-source-list-item__edit').simulate('click', new MockEvent());
+
+        expect(module.get('router').go).toHaveBeenCalledWith('data-sources.edit', { id: dataSource.id });
     });
 });
--- src/web/data-sources/DataSourceListItem.jsx
+++ src/web/data-sources/DataSourceListItem.jsx
@@ -1,11 +1,31 @@
 const React = require('react');
+const RestDataSource = require('../../shared/RestDataSource');
+const Module = require('../Module');
 
 class DataSourceListItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.router = props.module.get('router');
+    }
+    onClickEdit(e) {
+        e.preventDefault();
+        this.router.go('data-sources.edit', { id: this.props.dataSource.id });
+    }
     render() {
         return (<li className="data-source-list-item">
-            I'm  a data source
+            <h1 className="data-source-list-item__name">{this.props.dataSource.name}</h1>
+            <p className="data-source-list-item__url">{this.props.dataSource.url}</p>
+            <a href="" className="data-source-list-item__edit" onClick={e => this.onClickEdit(e)}>
+                Edit
+            </a>
         </li>);
     }
 }
+
+DataSourceListItem.propTypes = {
+    dataSource: React.PropTypes.instanceOf(RestDataSource).isRequired,
+    module: React.PropTypes.instanceOf(Module).isRequired,
+};
 
 module.exports = DataSourceListItem;
